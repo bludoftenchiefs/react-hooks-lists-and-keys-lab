6b43f487817960d303b12862dcbc880c3808e76c
rilith <bludoftenchiefs@protonmail.com>
2021-06-30 20:33:49 -0500

    commit tree 3336bdbe1f72fdc1e6b835f94ce89534164326a5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a href="#home" key={1}>home</a>
+    <a href="#about" key={2}>about</a>
+    <a href="#projects" key={3}>projects</a>
+  </nav>;
 }
 
 export default NavBar;
+
+// In the NavBar component, there is an array with three strings representing each link on the page. For each of those strings, 
+// create an <a> tag that looks like this:
+
+// <a href="#home">home</a> 
+// Make sure each <a> element also gets a unique key prop.
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const projectLists = technologies.map (technology => (
+    <span key={technology}>{technology}</span>
+  ))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className="technologies" >
+        {projectLists}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+
+// The ProjectItem component should get a prop of technologies passed down from ProjectList. For each element 
+// in this array of technologies, create a <span> tag that displays the name of the technology.
